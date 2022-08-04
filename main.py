341beaf70911db2fe4c20c03874ea1d6ed9d7f08
akrish6 <102596621+akrish6@users.noreply.github.com>
2022-08-04 14:56:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template      
+from flask import Flask, render_template, request
+#from chatterbot import ChatBot
+#from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
+#Flask initialization
 app = Flask(__name__)
+
+#chatbot=ChatBot('VA')
+# Create a new trainer for the chatbot
+#trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Now let us train our bot with multiple corpus
+#trainer.train("chatterbot.corpus.english.greetings", "chatterbot.corpus.english.conversations" )
 
 @app.route("/")
 def home():
@@ -13,6 +24,16 @@ def salvador():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/chatbot")
+def chatbot():
+    return render_template("chatbot.html")
+
+#@app.route("/get", methods=["GET","POST"])
+#def chatbot_response():
+    #msg = request.form["msg"]
+    #response = chatbot.get_response(msg)
+    #return str(response)
     
 if __name__ == "__main__":
     app.run(debug=True)
